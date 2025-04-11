6d33aac764323fb41d2579da049ebf5f5c58d3a5
Văn Thành <hackersvn1@gmail.com>
2025-04-12 05:26:53 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,16 +4,13 @@
 apk add --no-cache nodejs npm curl
 
 # Cài các thư viện cần thiết
-npm install express node-telegram-bot-api ngrok localtunnel
+npm install express node-telegram-bot-api localtunnel
 
 npm install -g localtunnel
 
-# Tải bot.js từ GitHub về
-curl -O https://raw.githubusercontent.com/neganok/API/main/bot.js
-
-
 # Chạy bot với MASTER_URL
-MASTER_URL=https://wild-doodles-sink.loca.lt node bot.js
+MASTER_URL=https://witty-nails-retire.loca.lt node bot.js
+
 
 
 
