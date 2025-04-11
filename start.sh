0322a11625ea57016503b3502bb86c3fca3f692a
Văn Thành <hackersvn1@gmail.com>
2025-04-11 12:27:01 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,13 @@
 apk add --no-cache nodejs npm curl
 
 # Cài các thư viện cần thiết
-npm install express node-telegram-bot-api ngrok
+npm install express node-telegram-bot-api ngrok localtunnel
+
+npm install -g localtunnel
 
 # Tải bot.js từ GitHub về
 curl -fsSL https://raw.githubusercontent.com/neganok/API/refs/heads/main/bot.js -o bot.js
 
 # Chạy bot với MASTER_URL
-MASTER_URL=https://4ed9-23-97-62-134.ngrok-free.app node bot.js
+MASTER_URL=https://every-planes-send.loca.lt node bot.js
+
