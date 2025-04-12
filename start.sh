ba0012306ea1a5e629600ef96afdaf5f67db900f
Văn Thành <hackersvn1@gmail.com>
2025-04-12 11:04:55 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,13 +2,9 @@
 
 # Cài nodejs + npm nếu chưa có
 apk add --no-cache nodejs npm curl bash
-
 # Cài các thư viện cần thiết
+
 npm install express node-telegram-bot-api localtunnel
 
 # Chạy bot với MASTER_URL
 MASTER_URL=https://plain-bats-end.loca.lt node bot.js
-
-
-
-
