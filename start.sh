685ee9b91b7134fb19f950014ac8030c46f9994e
Văn Thành <hackersvn1@gmail.com>
2025-04-12 11:04:17 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # Cài nodejs + npm nếu chưa có
-apk add --no-cache nodejs npm curl
+apk add --no-cache nodejs npm curl bash
 
 # Cài các thư viện cần thiết
 npm install express node-telegram-bot-api localtunnel
 
-
 # Chạy bot với MASTER_URL
-MASTER_URL=https://witty-nails-retire.loca.lt node bot.js
+MASTER_URL=https://plain-bats-end.loca.lt node bot.js
 
 
 
